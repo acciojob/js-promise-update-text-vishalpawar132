a45465197c8cb3c31b1ae99232df072750cb51dd
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-27 09:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function updateText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById("output");
+
+updateText()
+  .then((text) => {
+    outputElement.textContent = text;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
